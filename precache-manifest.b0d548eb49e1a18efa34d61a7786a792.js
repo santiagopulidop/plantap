2dbdeb18f294113471e5fcdfd94da6611aa1f8aa
Santiago Pulido <santiagopulidopelaez@gmail.com>
2020-05-28 09:14:22 -0500

    Updates

---
+++ precache-manifest.b0d548eb49e1a18efa34d61a7786a792.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "805c495cfedd3e2cd171b8c2201151fe",
+    "url": "/plantap/index.html"
+  },
+  {
+    "revision": "13551d682f425aac2a91",
+    "url": "/plantap/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "daf6045237e4b4b1b8fe",
+    "url": "/plantap/static/css/main.6b4532cf.chunk.css"
+  },
+  {
+    "revision": "13551d682f425aac2a91",
+    "url": "/plantap/static/js/2.e150fbdd.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/plantap/static/js/2.e150fbdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daf6045237e4b4b1b8fe",
+    "url": "/plantap/static/js/main.d19b6a17.chunk.js"
+  },
+  {
+    "revision": "aceb1a529e640c4f951b",
+    "url": "/plantap/static/js/runtime-main.4157bd5d.js"
+  },
+  {
+    "revision": "ba631b638722bb02c7a4ee1998ba6224",
+    "url": "/plantap/static/media/banner_img.ba631b63.jpg"
+  },
+  {
+    "revision": "0d3d721254ae1141a1533f413fcd768c",
+    "url": "/plantap/static/media/logo.0d3d7212.png"
+  }
+]);
